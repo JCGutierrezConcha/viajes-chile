5a27ad624770284a11efae1af704552c2d30dbc6
JCGutierrezConcha <jc.gutierrez.concha@gmail.com>
2024-02-17 11:38:05 -0300

    Agrega (document).ready en app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-$(function () {
+$(document).ready(function () {
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
