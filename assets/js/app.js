932d43cba92f1fabd6e25298e3ebf5b335bfae57
JCGutierrezConcha <jc.gutierrez.concha@gmail.com>
2024-02-17 02:50:25 -0300

    Agrega Contacto con form y tooltip y alerta al enviar mensaje

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,8 @@
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+$(function () {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+    $("#enviarMensaje").on('click', function () {
+        alert("El mensaje fue enviado correctamente...");
+    });
+});
